679ef504c6f2ac74f6607e75cc8e6f6a131fe3b7
Deák Dominik <deakdominik.2@gmail.com>
2022-03-11 09:48:02 +0100

    Handle numbers under a million

--- js-numerals/index.js
+++ js-numerals/index.js
@@ -4,16 +4,20 @@ const teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "si
 const tens = ["twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"];
 const thousands = ["thousand", "million", "billion", "trillion"];
 
-// var input = document.getElementById("inputField").value;
 
 function numberToWord(num) {
-  var number = num % 10; // To find the correct index
+  var number = num % 10; // To find the correct index of the singulars array
 
   if (num < 10) {
     return singulars[num];
   } else if (num >= 10 && num < 20) {
     return teens[number];
+  } else if (num < 100) {
+    return tens[Math.floor(num / 10) - 2] + (number? "-" + singulars[number]: "");  // for exmaple: 88 / 10 = 8.8 (~8) - 2 = 6 --> so the index is 6, which is "eighty" + 88 % 10 = 8 --> eighty-eight
+  } else if (num < 1000) {
+    return singulars[Math.floor(num / 100)] + " hundred" + (num % 100 == 0? "": " and " + numberToWord(num % 100));
   }
+    return numberToWord(Math.floor(num / 1000)) + " thousand" + (num % 1000 != 0? " " + numberToWord(num % 1000): "");
 
 }
 
@@ -21,6 +25,6 @@ function numberToWord(num) {
 document.getElementById("submit").addEventListener("click", function() {
 
   let number = document.getElementById("inputField").value;
-  
+
   document.getElementById("result").innerHTML = "Your number: " + numberToWord(number)
 });
